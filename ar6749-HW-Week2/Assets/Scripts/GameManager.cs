f9cd04e084f8530a1a95db9004616e7a81a1d277
Walid <ar6749@nyu.edu>
2021-02-22 16:12:12 -0500

    Week 2 homework with new notes

---
+++ ar6749-HW-Week2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private int score = 0;
+
+    private const string FILE_DEATHS = "/Logs/totalDeaths.txt";
+
+    string FILE_PATH_TOTAL_DEATHS; 
+    
+    const string PREF_KEY_TOTAL_DEATHS = "TotalDeathsKey";
+
+    public int Score
+    {
+        get { return score; }
+        set { score = value; }
+    }
+
+    public int targetscore = 3;
+
+    public Text text;
+
+    public int currentLevel = 0;
+
+    private int totalDeaths = -1;
+
+    private int deaths = 0;
+
+    public int Deaths
+    {
+        get { return deaths; }
+        set
+        {
+            deaths = value;
+            if (deaths > 0)
+            {
+                TotalDeaths ++;
+            }
+        }
+        
+    }
+
+    public int TotalDeaths
+    {
+        get
+        {
+            if (totalDeaths < 0)
+            {
+                //totalDeaths = PlayerPrefs.GetInt(PREF_KEY_TOTAL_DEATHS, 0);
+                string fileContents = File.ReadAllText(FILE_PATH_TOTAL_DEATHS);
+                totalDeaths = Int32.Parse(fileContents);
+
+            }   
+            return totalDeaths;
+        }
+        set
+        {
+            totalDeaths = value;
+            //PlayerPrefs.SetInt(PREF_KEY_TOTAL_DEATHS, totalDeaths);
+            File.WriteAllText(FILE_PATH_TOTAL_DEATHS, totalDeaths + "");
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        FILE_PATH_TOTAL_DEATHS = Application.dataPath + FILE_DEATHS;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Level: " + currentLevel +
+                    "\nScore: " + Score +
+                    "\nTarget: " + targetscore +
+                    "\nTotal deaths: " + TotalDeaths +
+                    "\nDeaths: " + Deaths;
+        
+        if (score == targetscore)
+        {
+            currentLevel++;
+            GetComponent<AudioSource>().Play();
+            targetscore += targetscore + targetscore/2;
+        }
+    }
+}
---
+++ ar6749-HW-Week2/Assets/Scripts/Spikes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Spikes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            GameManager.instance.Score = 0; 
+            GameManager.instance.targetscore = 3;
+            GameManager.instance.currentLevel = 0;
+            GameManager.instance.Deaths++;
+            
+            SceneManager.LoadScene(0);
+        }
+    }
+}
